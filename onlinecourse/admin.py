fbf8067c945d9e8707bb6ef96f26c0bec077daa4
Tshenolo Kab <tckabikwa@yahoo.com>
2026-02-03 14:21:13 -0500

    Add Question, Choice, Submission models and register them in admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,41 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+# Import all models
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
-
-
+# Inline classes to edit related models together
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 5
+    extra = 5  # Show 5 extra lesson forms by default
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2  # Show 2 extra choice forms by default
 
-# Register your models here.
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2  # Show 2 extra question forms by default
+
+# Admin class for Course
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline]  # Add lessons while creating a course
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+# Admin class for Question
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]  # Add choices while creating a question
+    list_display = ['content']  # Show question text in list view
 
+# Admin class for Lesson
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title']  # Show lesson title in list view
 
-
-# <HINT> Register Question and Choice models here
-
+# Register models with the admin site
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
